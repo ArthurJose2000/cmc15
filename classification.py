087cc29a54d14eacda6481d770cd4765264e6083
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2022-10-05 21:50:18 -0300

    Fix values

--- classification.py
+++ classification.py
@@ -16,19 +16,32 @@ classification_list = []
 
 for voter in voters:
   random_value = random()
-  
-  if (random_value > percentage_of_republicans):
-    # append real value  and a boolean to indicate if its correct
+  classification_value = {}
+
+  # append real data value
+  classification_value["voter"] = voter
+
+  if (random_value < percentage_of_republicans):
     # make a guess that the voter is republican
+    classification_value["guess"] = "republican"
+
     if (voter == "republican"):
-      classification_list.append({ voter, True })
+      # classification is correct
+      classification_value["is_answer_correct"] = True
     else:
-      classification_list.append({ voter, False })
+      # classification is wrong
+      classification_value["is_answer_correct"] = False
   else:
     # make a guess that the voter is democrat
+    classification_value["guess"] = "democrat"
+
     if (voter == "democrat"):
-      classification_list.append({ voter, True })
+      # classification is correct
+      classification_value["is_answer_correct"] = True
     else:
-      classification_list.append({ voter, False })
+      # classification is wrong
+      classification_value["is_answer_correct"] = False
+
+  classification_list.append(classification_value)
 
 print(classification_list)
